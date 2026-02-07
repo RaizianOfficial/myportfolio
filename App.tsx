e91af789f3ecf7c6d270a34b8b5c7f37a17a520a
Raizian Ofiicial <raizianofficial@gmail.com>
2026-02-07 17:45:34 +0530

    Add Certificate component to App layout

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Projects from './components/Projects';
+import Certificate from './components/Certificate';
 import About from './components/About';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
@@ -11,12 +13,15 @@ const App: React.FC = () => {
   return (
     <div className="min-h-screen bg-white text-black">
       <Navbar />
+
       <main>
         <Hero />
         <Projects />
+        <Certificate />
         <About />
         <Contact />
       </main>
+
       <Footer />
       <BottomNav />
     </div>
