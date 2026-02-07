f442fe3a8175af77a02c4cec95dfa68f01a13efb
Raizian Ofiicial <raizianofficial@gmail.com>
2026-02-07 17:49:52 +0530

    Fix import path for Certificate component

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Projects from './components/Projects';
-import Certificate from './components/Certificate';
+import Certificate from './components/certificate';
 import About from './components/About';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
